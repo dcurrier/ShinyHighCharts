82108d158184731ee2abfe0ca1c8a1d31cd2501a
Duane Currier <duane.currier@stjude.org>
2014-08-18 16:15:32 -0500

    Move highcharts output function to new file

---
+++ R/HighchartsOutput.R
@@ -0,0 +1,44 @@
+#' Highcharts Heatmap Output
+#'
+#' Render a renderHighHeatmap within an application page.
+#'
+#' @rdname highchartsOutput
+#'
+#' @param inputId
+#' @param width
+#' @param height
+#' @param type A character vector indicating the type of highchart to output
+#'
+#' @family shinyhighcharts elements
+#'
+#' @export
+highchartsOutput <- function(inputId, width="100%", height="400px", type=c("heatmap") ) {
+
+  type = match.arg(type)
+
+  if(type == "heatmap"){
+    JS = singleton(tags$head(
+      tags$script(src="shinyhighcharts/Highcharts-4.0.3/js/highcharts.js", type="text/javascript"),
+      tags$script(src="shinyhighcharts/Highcharts-4.0.3/js/modules/data.js", type="text/javascript"),
+      tags$script(src="shinyhighcharts/Highcharts-4.0.3/js/modules/exporting.js", type="text/javascript"),
+      tags$script(src="shinyhighcharts/Highcharts-4.0.3/js/modules/heatmap.src.js", type="text/javascript"),
+      tags$script(src="shinyhighcharts/heatmap-bindings.js", type="text/javascript")
+    ))
+
+    divClass = "shiny-bound-output highcharts-heatmap"
+  }
+
+
+  # Return html
+  tagList(
+    # Import the Highcharts library files
+    singleton(tags$head(tags$script(src="shinyhighcharts/Highcharts-4.0.3/js/modules/no-data-to-display.js"))),
+    JS,
+
+    # make the container div
+    div(id=inputId, class=divClass,
+        style=paste0("width: ", width, "; height: ", height, "; margin: 0 auto;"), list())
+
+  )
+
+}
