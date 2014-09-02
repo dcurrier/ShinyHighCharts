cc306c06dc33e89ee8a28864ad2afbe02ab2fe1a
Duane Currier <duane.currier@stjude.org>
2014-09-02 17:14:08 -0500

    inline require shiny

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  require(shiny)
-  addResourcePath("shinyhighcharts", system.file("www", package = "ShinyHighCharts"))
+  shiny::addResourcePath("shinyhighcharts", system.file("www", package = "ShinyHighCharts"))
 }
 
 .onAttach <- function(libname, pkgname) {
